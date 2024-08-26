1840c0d23e5942b44aeac59b54a11fadf172b0fd
mantop123 <hafizhaiman2009@gmail.com>
2024-08-26 16:03:17 +0800

    Create input.js

---
+++ input.js
@@ -0,0 +1,19 @@
+let inputDirection = {x: 0, y: 0 }
+let lastInputDirection = {x:0,y:0}
+window.addEventLIstener('keydown', eb =>{switch (e.key) {
+  case 'ArrowUp':
+    if (lastInputDirection,y !== 0) break
+    inputDirection={x:0y:-1}
+     break
+   case 'ArrowDown'
+     if (lastInputDirection.x !==0)break
+inoutDirection = {x: -1,y: 0}break
+  case 'ArrowLeft':
+if (lastIMPUTDIRECTION>Y !++ 0)break
+inputDirection = {x:-1, y: 0)
+break 
+case 'ArrowRight'
+if (lastInputDirection.x !==0)break
+inputDirection = {x:1,y:0}
+ break
+                                        }                                         
